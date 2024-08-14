6d953850ad2daea15588a0ce8194c4688c9c15ea
Tiven Pangtono <5632tivlai69@gmail.com>
2024-08-14 15:39:09 -0700

    did the forgot-password functionality via javascript

---
+++ src/frontend/Fitness Resouces/pages/forget-password.js
@@ -0,0 +1,21 @@
+document.getElementById('forgotPasswordForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message');
+
+    if(validateEmail(email)){
+        message.textContent = `A reset link has been sent do ${email}`;
+
+    } else{
+        message.textContent = "Please enter your valid email address.";
+        message.style.color = "red";
+    }
+
+});
+
+function validateEmail(email){
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+}
+    
